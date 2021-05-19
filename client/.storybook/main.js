03fafe7aecfee4fb806b58a030906fe31d4132e4
putskan <ben.lahav101@gmail.com>
2021-05-19 13:20:21 +0000

    add: storybook global scss

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -1,10 +1,16 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/stories/*.stories.mdx", "../src/stories/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+    "@storybook/addon-essentials",
+    '@storybook/preset-scss'
+  ],
+  webpackFinal: async (config, { configType }) => {
+    config.module.rules.push({
+      // this is for both css and scss
+      test: /.*\.(?:c|sc)ss$/,
+      loaders: ["css-loader", "resolve-url-loader", "sass-loader"],
+    })
+    return config
+  },
 }
