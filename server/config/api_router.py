a57ec614ab477b84e51cf2a1f287c242397bcef4
putskan <ben.lahav101@gmail.com>
2021-05-23 13:16:28 +0000

    add: School serializer, viewset

--- server/config/api_router.py
+++ server/config/api_router.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
 from server.users.api.views import UserViewSet
+from server.schools.api.views import SchoolViewSet
 
 if settings.DEBUG:
     router = DefaultRouter()
@@ -9,6 +10,7 @@ else:
     router = SimpleRouter()
 
 router.register("users", UserViewSet)
+router.register("schools", SchoolViewSet, "schools")
 
 
 app_name = "api"
