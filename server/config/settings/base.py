15a10c8d0213988d6e7a65d6f98bfe368ea5d59c
putskan <ben.lahav101@gmail.com>
2021-05-27 11:24:29 +0000

    remove: email verification mandatory

--- server/config/settings/base.py
+++ server/config/settings/base.py
@@ -293,7 +293,7 @@ ACCOUNT_AUTHENTICATION_METHOD = "email"
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
 ACCOUNT_EMAIL_REQUIRED = True
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
-ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+ACCOUNT_EMAIL_VERIFICATION = "none"
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
 ACCOUNT_ADAPTER = "server.users.adapters.AccountAdapter"
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
