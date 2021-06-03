d38532c5032a23790bb477cb9366d11c748df57c
putskan <ben.lahav101@gmail.com>
2021-06-03 13:05:53 +0000

    add: list_display to admin's orders view

--- server/server/organizations/admin.py
+++ server/server/organizations/admin.py
@@ -8,8 +8,13 @@ from .models import (
     SchoolActivityOrder,
 )
 
+
+@admin.register(SchoolActivityOrder)
+class SchoolActivityOrderAdmin(admin.ModelAdmin):
+    list_display = ["school", "activity", "created_at", "updated_at", "status"]
+
+
 admin.site.register(Organization)
 admin.site.register(Activity)
 admin.site.register(ActivityMedia)
 admin.site.register(OrganizationMember)
-admin.site.register(SchoolActivityOrder)
