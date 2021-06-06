3f598e8d68b05daf63a976b8be93fa12fd298250
putskan <ben.lahav101@gmail.com>
2021-06-06 13:51:55 +0000

    add: consumerActivity tests

--- server/server/organizations/tests/tests.py
+++ server/server/organizations/tests/tests.py
@@ -1,7 +1,9 @@
 import pytest
 from django.test import override_settings
+from rest_framework import status
 from rest_framework.test import APIClient
 
+from server.organizations.models import SchoolActivityGroup, SchoolActivityOrder
 from server.schools.models import SchoolMember
 
 pytestmark = pytest.mark.django_db
@@ -47,3 +49,70 @@ class TestManageSchoolProgramsView:
             url, {"school": school.slug, "activity": activity.slug}, format="json"
         )
         assert post_response.status_code == post_response_2.status_code == 400
+
+
+class TestConsumerActivityView:
+    @override_settings(DEBUG=True)
+    def test_consumer_can_view_only_approved_activities(
+        self, consumer, school, activity
+    ):
+        url = "/api/consumer_activities/"
+        SchoolMember.objects.create(user=consumer, school=school)
+
+        client = APIClient()
+        client.force_authenticate(user=consumer)
+        get_response_before_order = client.get(url)
+
+        order = SchoolActivityOrder.objects.create(activity=activity, school=school)
+        get_response_after_order_creation = client.get(url)
+
+        order.status = SchoolActivityOrder.Status.APPROVED
+        order.save()
+        get_response_after_order_approval = client.get(url)
+
+        assert (
+            status.HTTP_200_OK
+            == get_response_before_order.status_code
+            == get_response_after_order_creation.status_code
+            == get_response_after_order_approval.status_code
+        )
+        assert (
+            get_response_before_order.data["results"]
+            == get_response_after_order_creation.data["results"]
+            == []
+        )
+        assert len(get_response_after_order_approval.data["results"]) == 1
+
+    @override_settings(DEBUG=True)
+    def test_consumer_can_view_registration_status(self, consumer, school, activity):
+        """
+        test if registration status is true if in group, false otherwise
+        """
+        url = "/api/consumer_activities/"
+        SchoolMember.objects.create(user=consumer, school=school)
+        activity_order = SchoolActivityOrder.objects.create(
+            activity=activity, school=school, status=SchoolActivityOrder.Status.APPROVED
+        )
+        activity_group = SchoolActivityGroup.objects.create(
+            activity_order=activity_order
+        )
+
+        client = APIClient()
+        client.force_authenticate(user=consumer)
+        response_before_consumer_in_group = client.get(url)
+
+        activity_group.consumers.add(consumer)
+        response_after_consumer_in_group = client.get(url)
+
+        assert (
+            response_before_consumer_in_group.data["results"][0][
+                "is_consumer_registered"
+            ]
+            is False
+        )
+        assert (
+            response_after_consumer_in_group.data["results"][0][
+                "is_consumer_registered"
+            ]
+            is True
+        )
