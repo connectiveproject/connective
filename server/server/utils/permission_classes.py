e4bff939dda68e90e0ba71010915a92de81e1091
putskan <ben.lahav101@gmail.com>
2021-06-07 15:25:35 +0000

    fix: permission_classes 500 status code bug

--- server/server/utils/permission_classes.py
+++ server/server/utils/permission_classes.py
@@ -3,21 +3,33 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 class AllowCoordinator(BasePermission):
     def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+
         return request.user.user_type == request.user.Types.COORDINATOR
 
 
 class AllowConsumer(BasePermission):
     def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+
         return request.user.user_type == request.user.Types.CONSUMER
 
 
 class AllowVendor(BasePermission):
     def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+
         return request.user.user_type == request.user.Types.VENDOR
 
 
 class AllowCoordinatorReadOnly(BasePermission):
     def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+
         return (
             request.method in SAFE_METHODS
             and request.user.user_type == request.user.Types.COORDINATOR
@@ -26,7 +38,21 @@ class AllowCoordinatorReadOnly(BasePermission):
 
 class AllowConsumerReadOnly(BasePermission):
     def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+
         return (
             request.method in SAFE_METHODS
             and request.user.user_type == request.user.Types.CONSUMER
+        )
+
+
+class AllowVendorReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+
+        return (
+            request.method in SAFE_METHODS
+            and request.user.user_type == request.user.Types.VENDOR
         )
