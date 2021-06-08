b812d7069090d0bfd15c6044287dd1d350b0cfc5
thehiveril <thehiverisrael@gmail.com>
2021-06-08 15:10:02 +0000

    fix consumers field

--- server/server/events/models.py
+++ server/server/events/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 
+from server.organizations.models import SchoolActivityGroup
 from server.users.models import Consumer
 
-from server.organizations.models import SchoolActivityGroup
 
 class Event(models.Model):
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
-    consumers = models.ManyToManyField(Consumer, on_delete=models.SET_NULL)
-    school_group = models.ForeignKey(SchoolActivityGroup, on_delete=models.SET_NULL)
+    consumers = models.ManyToManyField(Consumer)
+    school_group = models.ForeignKey(
+        SchoolActivityGroup, on_delete=models.SET_NULL, null=True, blank=True
+    )
     locations_name = models.CharField(max_length=250, null=True, blank=True)
