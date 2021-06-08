bf812b73e8a79ee8a0707d096ebdf055058bd50a
thehiveril <thehiverisrael@gmail.com>
2021-06-08 11:39:26 +0000

    begin event model

--- server/server/events/models.py
+++ server/server/events/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from server.users.models import Consumer
+
+from server.organizations.models import SchoolActivityGroup
+
+class Event(models.Model):
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    consumers = models.ManyToManyField(Consumer, on_delete=models.SET_NULL)
+    school_group = models.ForeignKey(SchoolActivityGroup, on_delete=models.SET_NULL)
+    locations_name = models.CharField(max_length=250, null=True, blank=True)
