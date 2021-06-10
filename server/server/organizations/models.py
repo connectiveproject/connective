3fbb4c512e5ca35ff0468a782e837713a80ca108
putskan <ben.lahav101@gmail.com>
2021-06-10 16:27:58 +0000

    add: null & blank to model fields

--- server/server/organizations/models.py
+++ server/server/organizations/models.py
@@ -11,32 +11,42 @@ class Organization(models.Model):
     slug = models.CharField(max_length=40, default=random_slug, unique=True)
     email = models.EmailField()
     description = models.CharField(max_length=250)
-    website_url = models.URLField()
+    website_url = models.URLField(null=True, blank=True)
     name = models.CharField(max_length=50)
-    goal = models.CharField(max_length=250)
+    goal = models.CharField(max_length=250, null=True, blank=True)
     year_founded = models.CharField(max_length=4, null=True, blank=True)
-    status = models.CharField(max_length=50)
-    target_audience = models.JSONField()
-    number_of_employees = models.PositiveIntegerField()
-    number_of_members = models.PositiveIntegerField()
-    number_of_volunteers = models.PositiveIntegerField()
-    location_lon = models.DecimalField(max_digits=9, decimal_places=6)
-    location_lat = models.DecimalField(max_digits=9, decimal_places=6)
+    status = models.CharField(max_length=50, null=True, blank=True)
+    target_audience = models.JSONField(null=True, blank=True)
+    number_of_employees = models.PositiveIntegerField(null=True, blank=True)
+    number_of_members = models.PositiveIntegerField(null=True, blank=True)
+    number_of_volunteers = models.PositiveIntegerField(null=True, blank=True)
+    location_lon = models.DecimalField(
+        max_digits=9,
+        decimal_places=6,
+        null=True,
+        blank=True,
+    )
+    location_lat = models.DecimalField(
+        max_digits=9,
+        decimal_places=6,
+        null=True,
+        blank=True,
+    )
 
-    address_city = models.CharField(max_length=150)
-    address_street = models.CharField(max_length=150)
-    address_house_num = models.CharField(max_length=4)
-    address_zipcode = models.CharField(max_length=9)
-    cities = models.JSONField()
-    districts = models.JSONField()
-    union_type = models.CharField(max_length=50)
+    address_city = models.CharField(max_length=150, null=True, blank=True)
+    address_street = models.CharField(max_length=150, null=True, blank=True)
+    address_house_num = models.CharField(max_length=4, null=True, blank=True)
+    address_zipcode = models.CharField(max_length=9, null=True, blank=True)
+    cities = models.JSONField(null=True, blank=True)
+    districts = models.JSONField(null=True, blank=True)
+    union_type = models.CharField(max_length=50, null=True, blank=True)
 
 
 class Activity(models.Model):
     slug = models.CharField(max_length=40, default=random_slug, unique=True)
     name = models.CharField(max_length=35)
     target_audience = models.JSONField()
-    domain = models.CharField(max_length=55)
+    domain = models.CharField(max_length=55, null=True, blank=True)
     originization = models.ForeignKey(
         Organization, on_delete=models.SET_NULL, null=True, blank=True
     )
@@ -57,7 +67,7 @@ class Activity(models.Model):
 
 class ActivityMedia(models.Model):
     slug = models.CharField(max_length=40, default=random_slug, unique=True)
-    name = models.CharField(max_length=40)
+    name = models.CharField(max_length=40, null=True, blank=True)
     image_url = models.ImageField(blank=True, null=True)
     video_url = models.URLField(blank=True, null=True)
     activity = models.ForeignKey(
