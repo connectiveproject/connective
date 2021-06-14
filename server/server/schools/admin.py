f13a42a75297045b44a32cb8f21e81e08f1743dd
putskan <ben.lahav101@gmail.com>
2021-06-14 12:03:42 +0000

    add: school member to user creation

--- server/server/schools/admin.py
+++ server/server/schools/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from .models import School, SchoolMember
 
+
+class SchoolMemberTabularInline(admin.TabularInline):
+    model = SchoolMember
+    min_num = 1
+
+
 admin.site.register(School)
 admin.site.register(SchoolMember)
