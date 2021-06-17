5fc8000bbd5cf18c9e582a3a14473af0f2a52f5f
putskan <ben.lahav101@gmail.com>
2021-06-17 16:06:15 +0000

    add: overlay & fixed position to DetailModal

--- client/src/components/DetailModal.vue
+++ client/src/components/DetailModal.vue
@@ -1,23 +1,26 @@
 <template>
-  <v-card v-show="value" class="absolute-center z-index-5" max-width="344">
-    <v-card-text>
-      <p v-text="topSubtitle" />
-      <p class="text-h4 text--primary" v-text="title" />
-      <p v-text="bottomSubtitle" />
-      <div class="text--primary">
-        <slot></slot>
-      </div>
-    </v-card-text>
-    <v-card-actions>
-      <v-btn
-        text
-        class="mx-auto"
-        color="deep-purple accent-4"
-        v-text="buttonText"
-        @click="close"
-      />
-    </v-card-actions>
-  </v-card>
+  <div v-show="value">
+    <v-overlay />
+    <v-card class="fixed-center z-index-5" max-width="344">
+      <v-card-text>
+        <p v-text="topSubtitle" />
+        <p class="text-h4 text--primary" v-text="title" />
+        <p v-text="bottomSubtitle" />
+        <div class="text--primary">
+          <slot></slot>
+        </div>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn
+          text
+          class="mx-auto"
+          color="deep-purple accent-4"
+          v-text="buttonText"
+          @click="close"
+        />
+      </v-card-actions>
+    </v-card>
+  </div>
 </template>
 
 <script>
