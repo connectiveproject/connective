2b0dfad35d518287f700e8a9853b4c240ce22d75
putskan <ben.lahav101@gmail.com>
2021-06-22 15:18:27 +0000

    add: line-chart

---
+++ client/src/components/StyledLineChart.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="wrapper">
+    <line-chart v-bind="$attrs" />
+  </div>
+</template>
+
+<script>
+import LineChart from "./LineChart"
+export default {
+  inheritAttrs: false,
+  components: { LineChart },
+}
+</script>
+
+<style scoped>
+.wrapper {
+  max-width: 600px;
+  margin: 150px auto;
+}
+</style>
