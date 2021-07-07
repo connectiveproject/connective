b9539edde026c4c66966f691ccd655d6e75fa022
putskan <ben.lahav101@gmail.com>
2021-07-07 09:06:48 +0000

    add: sentry

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,6 @@
 import Vue from "vue"
+import * as Sentry from "@sentry/vue"
+import { Integrations } from "@sentry/tracing"
 import App from "./App.vue"
 import router from "./router"
 import store from "./vuex/store"
@@ -14,6 +16,15 @@ import "./filters"
 Vue.config.productionTip = false
 Vue.use(cookies)
 Api.config.initAxiosSettings()
+
+Sentry.init({
+  Vue: Vue,
+  dsn: "https://39d20f84436748028c8282bd7657ae61@o567262.ingest.sentry.io/5852636",
+  integrations: [new Integrations.BrowserTracing()],
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+})
 
 new Vue({
   vuetify,
