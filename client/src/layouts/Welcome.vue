3bc8dcd720a1dc5427cb6388f7117397c3f093c6
putskan <ben.lahav101@gmail.com>
2021-07-13 08:15:53 +0000

    add: hashalem vid

--- client/src/layouts/Welcome.vue
+++ client/src/layouts/Welcome.vue
@@ -1,18 +1,24 @@
 <template>
-  <div :style="style" class="d-flex justify-center">
-    <router-view />
-  </div>
+  <v-row>
+    <v-col lg="4">
+      <router-view />
+    </v-col>
+    <v-col lg="8">
+      <video :src="video" autoplay loop :style="style" />
+    </v-col>
+  </v-row>
 </template>
 <script>
 import { BACKGROUNDS } from "../helpers/constants/images"
 export default {
   data() {
     return {
-      style: {
-        background: `url(${BACKGROUNDS.welcome})`,
-        "background-size": "cover",
-        "min-height": "100vh",
-      },
+      video: BACKGROUNDS.welcome,
+      // style: {
+      //   background: `url(${BACKGROUNDS.welcome})`,
+      //   "background-size": "cover",
+      //   "min-height": "100vh",
+      // },
     }
   },
 }
