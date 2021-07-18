c773c0a1555f715e6c17f90b0027d5d61e20dfad
putskan <ben.lahav101@gmail.com>
2021-07-18 14:40:13 +0000

    fix: input-drawer collapse on v-select clicks

--- client/src/components/InputDrawer.vue
+++ client/src/components/InputDrawer.vue
@@ -2,7 +2,10 @@
   <div
     class="drawer white-bg pt-3"
     @click="openDrawer"
-    v-click-outside="closeDrawer"
+    v-click-outside="{
+      handler: closeDrawer,
+      include: clickOutsideInclude,
+    }"
   >
     <v-row dense justify="space-between">
       <v-col cols="2" sm="2">
@@ -31,7 +34,7 @@
             <v-select
               v-if="type === 'select'"
               v-show="isDrawerOpen()"
-              class="mt-5"
+              class="included mt-5"
               :value="value"
               @input="$emit('input', $event)"
               :error-messages="errors"
@@ -39,8 +42,7 @@
               :multiple="multiselect"
               chips
               deletable-chips
-            >
-            </v-select>
+            />
             <strong
               v-show="!isDrawerOpen()"
               class="text-subtitle-2 text-lg-subtitle-1"
@@ -103,9 +105,11 @@ export default {
     },
   },
 
-  data: () => ({
-    drawerOpened: false,
-  }),
+  data() {
+    return {
+      drawerOpened: false,
+    }
+  },
 
   methods: {
     openDrawer() {
@@ -116,6 +120,10 @@ export default {
     },
     isDrawerOpen() {
       return this.drawerOpened
+    },
+    clickOutsideInclude() {
+      // don't collapse input-drawer if click happened inside v-select
+      return [...document.getElementsByClassName("menuable__content__active")]
     },
   },
 
