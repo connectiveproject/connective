0823d26596be4881d1f6026f4d5f5a1b6b41b2fb
Ronnie Sheer <ronysheer@gmail.com>
2021-07-18 14:54:58 +0300

    begin tribute

--- client/src/views/InstructorEventSummary.vue
+++ client/src/views/InstructorEventSummary.vue
@@ -16,11 +16,15 @@
     <form class="form" @submit.prevent="onSubmit">
       <v-row>
         <v-col cols="12">
-          <v-text-field
-            :label="$t('events.summaryGeneralNotes')"
-            v-model="summaryGeneralNotes"
-            class="my-6"
-          />
+          <vue-tribute :options="tributeOptions">
+            <v-textarea
+              :label="$t('events.summaryGeneralNotes')"
+              v-model="summaryGeneralNotes"
+              class="my-6"
+            >
+            </v-textarea>
+          </vue-tribute>
+
         </v-col>
         <v-col cols="12" sm="12" lg="6">
           <v-select
@@ -69,13 +73,86 @@
 
 <script>
 import { mapActions } from "vuex"
+// import VueTribute from "vue-tribute"
+import Tribute from "tributejs"
 import store from "../vuex/store"
 import Utils from "../helpers/utils"
 import TitleToText from "../components/TitleToText"
 import Modal from "../components/Modal"
+// disable eslint for file
+/* eslint-disable */
+const VueTribute = {
+  name: "vue-tribute",
+  props: {
+    options: {
+      type: Object,
+      required: true
+    }
+  },
+  watch: {
+    options: {
+      immediate: false,
+      deep: true,
+      handler() {
+        if (this.tribute) {
+          setTimeout(() => {
+            var $el = this.$slots.default[0].elm;
+            console.log($el);
+            this.tribute.detach($el);
+
+            setTimeout(() => {
+              $el = this.$slots.default[0].elm;
+              this.tribute = new Tribute(this.options);
+              this.tribute.attach($el);
+              $el.tributeInstance = this.tribute;
+            }, 0);
+          }, 0);
+        }
+      }
+    }
+  },
+  mounted() {
+    if (typeof Tribute === "undefined") {
+      throw new Error("[vue-tribute] cannot locate tributejs!");
+    }
+
+    const $el = document.querySelectorAll("textarea")[0];
+
+    this.tribute = new Tribute(this.options);
+
+    this.tribute.attach($el);
+
+    $el.tributeInstance = this.tribute;
+
+    $el.addEventListener("tribute-replaced", e => {
+      e.target.dispatchEvent(new Event("input", { bubbles: true }));
+    });
+  },
+  beforeDestroy() {
+    const $el = document.querySelectorAll("textarea")[0];
+
+    if (this.tribute) {
+      this.tribute.detach($el);
+    }
+  },
+  render(h) {
+    return h(
+      "div",
+      {
+        staticClass: "v-tribute"
+      },
+      this.$slots.default
+    );
+  }
+};
+
+if (typeof window !== "undefined" && window.Vue) {
+  window.Vue.component(VueTribute.name, VueTribute);
+}
+
 
 export default {
-  components: { TitleToText, Modal },
+  components: { TitleToText, Modal, VueTribute },
   props: {
     slug: {
       type: String,
@@ -91,15 +168,33 @@ export default {
       "instructorProgramGroup/getConsumers",
       event.schoolGroup
     )
+    console.table(consumers);
     next(vm => {
       vm.event = event
       vm.consumerchoices = consumers.map(c => ({ text: c.name, value: c.slug }))
       vm.attendedConsumers = consumers.map(c => c.slug)
+      vm.tributeOptions.values = consumers.map(consumer => {
+        return {
+          key: consumer.name,
+          value: consumer.name.replace(" ", "_")
+        };
+      });
     })
   },
   data() {
     return {
       event: {},
+      tributeOptions: {
+        trigger: "@",
+        values: [
+          { key: "Collin Henderson", value: "syropian" },
+          { key: "Sarah Drasner", value: "sarah_edo" },
+          { key: "Evan You", value: "youyuxi" },
+          { key: "Adam Wathan", value: "adamwathan" }
+        ],
+        positionMenu: true,
+        menuContainer: document.querySelector(".menu-container")
+      },
       consumerchoices: [],
       attendedConsumers: [],
       summaryGeneralNotes: "",
@@ -126,3 +221,62 @@ export default {
   },
 }
 </script>
+<style lang="scss">
+  div.tribute-container > ul > li {
+    -webkit-text-size-adjust: 100%;
+    word-break: normal;
+    tab-size: 4;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -webkit-tap-highlight-color: rgba(0,0,0,0);
+    font-family: Roboto,sans-serif;
+    text-align: center!important;
+    box-sizing: inherit;
+    align-items: center;
+    cursor: default;
+    display: inline-flex;
+    line-height: 20px;
+    max-width: 100%;
+    outline: none;
+    overflow: hidden;
+    padding: 0 12px;
+    position: relative;
+    text-decoration: none;
+    transition-duration: .28s;
+    transition-property: box-shadow,opacity;
+    transition-timing-function: cubic-bezier(.4,0,.2,1);
+    vertical-align: middle;
+    white-space: nowrap;
+    margin: 8px!important;
+    border-radius: 16px;
+    font-size: 14px;
+    height: 32px;
+    background-color: #5cbbf6 !important;
+    border-color: #5cbbf6 !important;
+    color: #fff;
+    background: #e0e0e0;
+    // -webkit-text-size-adjust: 100%;
+    // word-break: normal;
+    // tab-size: 4;
+    // text-rendering: optimizeLegibility;
+    // -webkit-font-smoothing: antialiased;
+    // -webkit-tap-highlight-color: rgba(0,0,0,0);
+    // font-family: Roboto,sans-serif;
+    // text-align: center!important;
+    // cursor: default;
+    // line-height: 20px;
+    // white-space: nowrap;
+    // font-size: 14px;
+    // color: #fff;
+    // background-repeat: no-repeat;
+    // box-sizing: inherit;
+    // padding: 0;
+    // margin: 0;
+    // align-items: center;
+    // display: inline-flex;
+    // height: 100%;
+    // max-width: 100%;
+   }
+
+
+</style>
--- server/server/events/admin.py
+++ server/server/events/admin.py
@@ -1,6 +1,10 @@
+import logging
+
 from django.contrib import admin
 
 from .models import ConsumerEventFeedback, Event
+
+logger = logging.getLogger(__name__)
 
 
 @admin.register(Event)
@@ -8,9 +12,16 @@ class EventAdmin(admin.ModelAdmin):
     list_display = ["school_group", "start_time", "end_time", "school", "activity"]
 
     def school(self, obj):
-        return obj.school_group.activity_order.school
+        try:
+            return obj.school_group.activity_order.school
+        except AttributeError as err:
+            logger.warn(err)
 
     def activity(self, obj):
-        return obj.school_group.activity_order.activity
+        try:
+            return obj.school_group.activity_order.activity
+        except AttributeError as err:
+            logger.warn(err)
+
 
 admin.site.register(ConsumerEventFeedback)
