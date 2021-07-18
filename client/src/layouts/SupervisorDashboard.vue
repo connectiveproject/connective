959a4682f077c8c63fbcee0d250482bff45eced8
NsypX <darkdino12@gmail.com>
2021-07-18 12:32:24 +0000

    added_supervisor_client_side

---
+++ client/src/layouts/SupervisorDashboard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div :style="style">
+    <navbar userType="Supervisor"/>
+    <router-view/>
+  </div>
+</template>
+<script>
+import { BACKGROUNDS } from "../helpers/constants/images"
+import Navbar from "../components/Navbar/Navbar"
+
+export default {
+  components: {
+    Navbar,
+  },
+  data() {
+    return {
+      style: {
+        background: `url(${BACKGROUNDS.managementDashboard})`,
+        "min-height": "100vh",
+      },
+    }
+  },
+}
+</script>
