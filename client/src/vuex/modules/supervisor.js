f2bfe1338940e651b4a1d2938d94dce35cb02e7e
NsypX <darkdino12@gmail.com>
2021-07-19 14:10:06 +0000

    added_serlizer_supervisor

---
+++ client/src/vuex/modules/supervisor.js
@@ -0,0 +1,45 @@
+import Api from "../../api"
+
+function getDefaultState() {
+  return {
+    profile: {
+      phoneNumber: null,
+      profilePicture: null,
+      jobDescription: null,
+    },
+  }
+}
+
+const supervisor = {
+  namespaced: true,
+  state: getDefaultState(),
+  mutations: {
+    FLUSH_STATE(state) {
+      Object.assign(state, getDefaultState())
+    },
+    SET_PROFILE(state, userProfile) {
+      state.profile = userProfile
+    },
+  },
+  actions: {
+    flushState({ commit }) {
+      commit("FLUSH_STATE")
+    },
+    async getProfile({ commit, state }) {
+      if (!state.profile.phoneNumber) {
+        // fetch if not in cache
+        let res = await Api.supervisor.getProfile()
+        commit("SET_PROFILE", res.data)
+      }
+      return state.profile
+    },
+    async updateProfile({ commit, state }, { slug, profile }) {
+      let res = await Api.supervisor.updateProfile(slug, profile)
+      commit("SET_PROFILE", res.data)
+      return state.profile
+    },
+  },
+}
+
+// i.e., principal
+export default supervisor
