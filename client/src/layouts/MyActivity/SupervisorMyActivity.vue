ccd7dc3770753bab36c4cb9bc4aa83643c9c5ee2
NsypX <darkdino12@gmail.com>
2021-07-21 13:26:50 +0000

    adding_supervisor_programlst

---
+++ client/src/layouts/MyActivity/SupervisorMyActivity.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="pa-10">
+    <route-tabs :tabs="tabs" />
+    <v-row>
+      <v-col class="mx-auto" sm="11" lg="9">
+        <router-view />
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import RouteTabs from "../../components/RouteTabs"
+
+export default {
+  components: { RouteTabs },
+  data() {
+    return {
+      tabs: [
+        {
+          componentName: "ConsumerMyGroups",
+          text: this.$t("myActivity.activeGroups"),
+        },
+        {
+          componentName: "ConsumerMyEvents",
+          text: this.$t("events.eventsBoard"),
+        },
+        {
+          componentName: "ConsumerPendingEventsFeedback",
+          text: this.$t("events.eventsFeedback"),
+        },
+      ],
+    }
+  },
+}
+</script>
