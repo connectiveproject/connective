24b3b97f13b954abed3a5f7ed7f65822283f2ad9
Yuval Siev <yuval.s@visiblerisk.com>
2021-07-21 19:23:02 +0300

    add initial gets

--- server/server/posts/api/views.py
+++ server/server/posts/api/views.py
@@ -1,10 +1,43 @@
+from django.contrib.auth import get_user_model
 from rest_framework import viewsets
 
 from server.posts.models import Post
 from server.posts.api.serializers import PostSerializer
 
+from server.server.events.models import Event
+from server.server.organizations.models import SchoolActivityGroup
+
 
 class PostViewSet(viewsets.ModelViewSet):
     lookup_field = "slug"
     serializer_class = PostSerializer
-    queryset = Post.objects.all()
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.user_type == get_user_model().Types.CONSUMER:  # student
+            pass
+        elif user.user_type == get_user_model().Types.INSTRUCTOR:
+            return Post.objects.filter(
+                event=Event.objects.filter(school_group=
+                    SchoolActivityGroup.objects.filter(
+                        instructor=user,
+                    )
+                )
+            )
+        elif user.user_type == get_user_model().Types.COORDINATOR:  # school manager
+            pass
+        elif user.user_type == get_user_model().Types.SUPERVISOR:
+            return Post.objects.all()
+
+        return []
+
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user,
+        )
+
+    def perform_update(self, serializer):
+        serializer.save(
+            author=self.request.user,
+        )
