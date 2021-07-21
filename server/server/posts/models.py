08e693d344f76887c38fa5f67efaa971463c8192
yuvalsiev96 <yuval.s@visiblerisk.com>
2021-07-21 14:39:31 +0000

    fix model

--- server/server/posts/models.py
+++ server/server/posts/models.py
@@ -1,14 +1,8 @@
 from django.contrib.postgres.fields import ArrayField
-from django.db.models import (
-    Model,
-    ForeignKey,
-    CharField,
-    SET_NULL,
-    TextField
-)
+from django.db.models import SET_NULL, CharField, ForeignKey, Model, TextField
 
-from server.users.models import User
 from server.events.models import Event
+from server.users.models import User
 from server.utils.model_fields import random_slug
 
 
@@ -31,4 +25,5 @@ class Post(Model):
     post_content = TextField()
     images_b64 = ArrayField(
         TextField(),
+        null=True,
     )
