ca439ba5c5faedb17f5ed7504a139d7a68b1e100
Yuval Siev <yuval.s@visiblerisk.com>
2021-07-21 14:41:43 +0300

    add initial posts app

---
+++ server/server/posts/models.py
@@ -0,0 +1,42 @@
+from django.db.models import (
+    Model,
+    ForeignKey,
+    CharField,
+    SET_NULL,
+    ManyToManyField,
+)
+
+from server.server.events.models import Event
+from server.server.schools.models import random_slug
+from server.server.users.models import User
+
+
+class Image(Model):
+    slug = CharField(
+        max_length=40,
+        default=random_slug,
+        unique=True,
+    )
+    image_b64 = CharField()
+
+
+class Post(Model):
+    slug = CharField(
+        max_length=40,
+        default=random_slug,
+        unique=True,
+    )
+    event = ForeignKey(
+        to=Event,
+        on_delete=SET_NULL,
+        null=True,
+    )
+    author = ForeignKey(
+        to=User,
+        on_delete=SET_NULL,
+        null=True,
+    )
+    post_content = CharField()
+    images = ManyToManyField(
+        to=Image,
+    )
