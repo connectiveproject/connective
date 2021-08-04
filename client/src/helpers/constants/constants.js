56f059d0679c7a55d91e0d43dfda0034f1ff6680
putskan <ben.lahav101@gmail.com>
2021-08-04 08:37:14 +0000

    add: 07x phone number validation support, modify email template

--- client/src/helpers/constants/constants.js
+++ client/src/helpers/constants/constants.js
@@ -77,7 +77,7 @@ export const TOKEN_COOKIE_NAME = "token"
 export const PASSWORD_REGEX_PATTERN =
   "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})"
 export const ISRAELI_PHONE_REGEX_PATTERN =
-  "^0(([23489]{1}\\d{7})|[5]{1}\\d{8})$"
+  "^0(([23489]{1}\\d{7})|[57]{1}\\d{8})$"
 export const EMAIL_REGEX_PATTERN =
   '^(([^<>()[\\]\\.,;:\\s@\\"]+(\\.[^<>()[\\]\\.,;:\\s@\\"]+)*)|(\\".+\\"))@(([^<>()[\\]\\.,;:\\s@\\"]+\\.)+[^<>()[\\]\\.,;:\\s@\\"]{2,})$'
 export const WEBSITE_REGEX_PATTERN =
