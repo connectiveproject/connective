e86017f78e45f08b16a038be821afffea4937dfd
putskan <ben.lahav101@gmail.com>
2021-08-11 15:27:02 +0000

    add: padding

--- client/src/layouts/Welcome.vue
+++ client/src/layouts/Welcome.vue
@@ -1,6 +1,6 @@
 <template>
   <v-row no-gutters>
-    <v-col class="secondary relative" cols="12" lg="5">
+    <v-col class="px-2 secondary relative" cols="12" lg="5">
       <router-view />
     </v-col>
     <v-col class="relative d-none d-lg-block overflow-hidden" lg="7">
