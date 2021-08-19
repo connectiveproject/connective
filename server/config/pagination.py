46dadb69feb7ad082d60c0a9e735f51b47fb7aed
putskan <ben.lahav101@gmail.com>
2021-08-19 08:40:57 +0000

    change: pagination page size

--- server/config/pagination.py
+++ server/config/pagination.py
@@ -2,6 +2,6 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class DefaultPagination(PageNumberPagination):
-    page_size = 300
+    page_size = 10
     page_size_query_param = "page_size"
     max_page_size = 1000
