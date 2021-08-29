38e525d38e07f2f42a6cd3554860855dd6bd0ba4
putskan <ben.lahav101@gmail.com>
2021-08-29 13:19:40 +0000

    remove: flush pagi guard

--- client/src/plugins/i18n.js
+++ client/src/plugins/i18n.js
@@ -4,14 +4,18 @@ import VueI18n from "vue-i18n"
 Vue.use(VueI18n)
 
 function loadLocaleMessages() {
-  // return an object containing translations from specified locale JSON files
-  const fileNames = ["he"]
-  let messages = {}
-  for (let name of fileNames) {
-    messages[name] = require(`../locales/${name}.json`)
-  }
-  return messages
+  const locales = require.context('@/locales', true, /[A-Za-z0-9-_,\s]+\.json$/i);
+  const messages = {};
+  locales.keys().forEach(key => {
+    const matched = key.match(/([A-Za-z0-9-_]+)\./i);
+    if (matched && matched.length > 1) {
+      const locale = matched[1];
+      messages[locale] = locales(key);
+    }
+  });
+  return messages;
 }
+
 
 export default new VueI18n({
   locale: "he",
