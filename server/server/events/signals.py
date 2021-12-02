a86f6d9c8b426f29985ad343464c1e06ad95e77a
putskan <ben.lahav101@gmail.com>
2021-12-02 10:36:50 +0200

    feat: event creation analytics (#137)

--- server/server/events/signals.py
+++ server/server/events/signals.py
@@ -1,22 +1,50 @@
 from datetime import timedelta
 
+import analytics
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from server.utils.analytics_utils import event as analytics_event
+from server.utils.analytics_utils import field as analytics_field
+
 from .models import Event, EventOrder
+
+
+def _track_events_creation(events):
+    """
+    track events creation using analytics
+    :param events: list of Event objects to track
+    """
+    for e in events:
+        analytics.track(
+            analytics_field.UNKNOWN_SLUG,
+            analytics_event.EVENT_CREATED,
+            {
+                "source": analytics_field.EVENT_CREATION_SOURCE_ACTIVITY_ORDER_APPROVAL,
+                "slug": e.slug,
+                "event_order_slug": e.event_order.slug,
+                "start_time": e.start_time,
+                "end_time": e.end_time,
+                "school_group_slug": e.school_group.slug,
+                "locations_name": e.locations_name,
+                "is_canceled": e.is_canceled,
+            },
+        )
 
 
 @receiver(post_save, sender=EventOrder)
 def create_events_on_order_approval(sender, instance, created, **kwargs):
     if instance.status == EventOrder.Status.APPROVED and instance.events.count() == 0:
         if instance.recurrence == EventOrder.Recurrence.ONE_TIME:
-            return Event.objects.create(
+            e = Event.objects.create(
                 school_group=instance.school_group,
                 locations_name=instance.locations_name,
                 start_time=instance.start_time,
                 end_time=instance.end_time,
                 event_order=instance,
             )
+            _track_events_creation([e])
+            return
 
         events_to_create = []
         for i in range(0, 52):
@@ -32,6 +60,7 @@ def create_events_on_order_approval(sender, instance, created, **kwargs):
                 )
             )
         Event.objects.bulk_create(events_to_create)
+        _track_events_creation(events_to_create)
 
 
 @receiver(post_save, sender=EventOrder)
