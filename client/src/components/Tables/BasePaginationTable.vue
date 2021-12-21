51b2596dddfb500a359a3215f25885e27d3b0d8b
Evyatar Cohen <evyatarcohen@Evyatars-Air.lan>
2021-12-21 10:53:56 +0200

    feat: add filter to pagination table

--- client/src/components/Tables/BasePaginationTable.vue
+++ client/src/components/Tables/BasePaginationTable.vue
@@ -1,27 +1,31 @@
 <template>
   <v-card class="pt-3" :elevation="elevation">
-    <v-text-field
-      v-if="!hideSearch"
-      v-model="searchFilter"
-      data-testid="table-searchbar"
-      class="search-bar px-10 mt-5 mb-8 mx-auto"
-      append-icon="mdi-magnify"
-      single-line
-      hide-details
-      :label="$t('userActions.search')"
-      @click:append="onSearch"
-      @keyup.enter="onSearch"
-    />
+    <v-container fluid>
+      <v-row>
+        <v-col class="d-flex" cols="12" align="center" align-self="center">
+          <v-text-field
+            v-if="!hideSearch"
+            v-model="searchFilter"
+            data-testid="table-searchbar"
+            class="search-bar px-10 mt-5 mb-8 mx-auto"
+            append-icon="mdi-magnify"
+            hide-details
+            :label="$t('userActions.search')"
+            @click:append="onSearch"
+            @keyup.enter="onSearch"
+          />
 
-    <v-select
-      v-if="filter1Field"
-      v-model="filter1Value"
-      class="search-bar px-10 mt-5 mb-8 mx-auto"
-      @change="onSearch"
-      :label="filter1Label"
-      :items="filter1Items"
-    />
-
+          <v-select
+            v-if="filter1Field"
+            v-model="filter1Value"
+            class="search-bar px-10 mt-5 mb-8 mx-auto"
+            @change="onSearch"
+            :label="filter1Label"
+            :items="filter1Items"
+          />
+        </v-col>
+      </v-row>
+    </v-container>
     <v-data-table
       multi-sort
       v-bind="$attrs"
@@ -86,15 +90,21 @@ export default {
       default: false,
     },
     filter1Label: {
+      // label to show for the filter (e.g. display name of the field to filter)
       type: String,
       default: "Filter 1",
     },
     filter1Field: {
+      // name of the field to filter - will be used in the API (after conversion from camel to snake case)
       type: String,
     },
     filter1Items: {
+      // array of options to show in the filter. Should include text (to display) and value (to send in the API)
       type: Array,
-      default: () => ["Filter Option 1", "Filter Option 2"],
+      default: () => [
+        { text: "Example Filter Option 1", value: "EXAMPLE_OPTION_1" },
+        { text: "Example Filter Option 2", value: "EXAMPLE_OPTION_2" },
+      ],
     },
   },
   data() {
