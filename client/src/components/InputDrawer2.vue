75d902ce93dc0028f674c7f4a6c9e3ef89f84687
putskan <ben.lahav101@gmail.com>
2022-02-28 12:48:58 +0200

    Input component (#192)
    * add: new input drawer component
    
    * remove: console.log
    
    * add: props required attr

---
+++ client/src/components/InputDrawer2.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <v-row v-if="!editMode">
+      <v-col>
+        <h3 v-text="value || label" class="font-weight-regular" />
+      </v-col>
+      <v-col>
+        <v-btn class="float-end" icon @click="editMode = true">
+          <v-icon>mdi-pen</v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-text-field
+      v-else
+      v-click-outside="onClickOutsideField"
+      v-model="activeValue"
+      class="mt-5"
+      :label="label"
+    >
+      <v-btn icon @click="onSave" slot="append">
+        <v-icon>mdi-content-save</v-icon>
+      </v-btn>
+    </v-text-field>
+    <modal-approve
+      v-model="isUnsavedChangesModalOpen"
+      @approve="onSave"
+      @disapprove="onDiscard"
+    >
+      {{
+        this.$t(
+          "confirm.someDetailsWereEditedButNotSaved-WouldYouLikeToSaveThem?"
+        )
+      }}
+    </modal-approve>
+  </div>
+</template>
+
+<script>
+import ModalApprove from "@/components/ModalApprove"
+
+export default {
+  components: {
+    ModalApprove,
+  },
+  props: {
+    value: {
+      // note: this value updates only on save
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      editMode: false,
+      isUnsavedChangesModalOpen: false,
+      activeValue: this.value,
+    }
+  },
+  methods: {
+    onClickOutsideField() {
+      if (this.value !== this.activeValue) {
+        this.isUnsavedChangesModalOpen = true
+      } else {
+        this.editMode = false
+      }
+    },
+    onSave() {
+      this.editMode = false
+      this.$emit("input", this.activeValue)
+    },
+    onDiscard() {
+      this.editMode = false
+
+      this.activeValue = this.value
+    },
+  },
+}
+</script>
